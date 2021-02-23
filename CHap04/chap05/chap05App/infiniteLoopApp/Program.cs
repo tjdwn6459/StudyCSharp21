2338c0b004f13fc9accce90f486042a7d3610fcf
tjdwn6459 <tjdwn6459@naver.com>
2021-02-23 15:15:25 +0900

    23일 오후 커밋

---
+++ CHap04/chap05/chap05App/infiniteLoopApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace infiniteLoopApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            long idx = 0;
+            for (; ; )
+            while (true)
+                {
+                    Console.WriteLine($"idx = { idx++}");
+                }
+        }
+    }
+}
