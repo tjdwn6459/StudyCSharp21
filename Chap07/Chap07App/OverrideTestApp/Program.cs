6244b28d4727c2265a6b834b9de4843199add395
tjdwn6459 <tjdwn6459@naver.com>
2021-02-25 12:31:00 +0900

    7장 완료

---
+++ Chap07/Chap07App/OverrideTestApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverrideTestApp
+{
+    class ArmorSuite
+    {
+        //자식클래스에서 재정의할꺼야.
+        public virtual void Initialize()
+        {
+            Console.WriteLine("ArmorSuite 초기화");
+        }
+    }
+
+
+    class IronMan : ArmorSuite
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Console.WriteLine("리펄서 레이저 초기화");
+
+        }
+
+
+    }
+
+    class WarMachine : ArmorSuite
+    {
+        public override void Initialize()
+        {
+            Console.WriteLine("AmorSuite 개량 Warmachine 초기화");
+            Console.WriteLine("Double-Barrel 케논 장착");
+            Console.WriteLine("Micro-Rocket 런처 장착");
+
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ArmorSuite 생산");
+            ArmorSuite suite = new ArmorSuite();
+            suite.Initialize();
+
+            Console.WriteLine("WarMachine 생산");
+            WarMachine machine = new WarMachine();
+            machine.Initialize();
+
+            Console.WriteLine("IronMan 생산");
+            IronMan ironMan = new IronMan();
+            ironMan.Initialize();
+        }
+    }
+}
