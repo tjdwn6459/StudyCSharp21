5e2af8613359e6dc3cc41eaf7bb3295800c24847
tjdwn6459 <tjdwn6459@naver.com>
2021-03-03 10:26:25 +0900

    13장 대리자 이벤트

---
+++ Chap13/ChapApp13/DelegateChain2App/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateChain2App
+{
+    class Program
+    {
+        delegate void ThereISAFire(string location); //대리자 선언
+
+        static void Call119(string location)//대리자 할 일 부여
+        {
+
+            Console.WriteLine($"소방서죠? 불났어요! 주소는 {location} 예요!");
+         }   
+        static void ShoutOut(string location)
+        {
+            Console.WriteLine($"불이야! {location}에 불났어요!");
+        }
+
+        static void Escape(string location)
+        {
+            Console.WriteLine($"{location}에서 나갑니다.");
+        }
+        static void Main(string[] args) //위에 부여한 일 부르는 순서대로 실행가능
+        {
+            ThereISAFire fire = new ThereISAFire(ShoutOut);
+            fire += new ThereISAFire(Call119);
+            fire += Escape;
+
+                fire("문현동 전광빌라");
+        }
+    }
+}
---
+++ Chap13/ChapApp13/UsingEventApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace UsingEventApp
+{
+    delegate void EventHandler(string message);//메세지를 받아서 처리하는 대리자 선언
+
+    class CustomNotifier
+    {//이벤트 선언, 사용하는 객체
+        public event EventHandler SomethingHAppened;//" SomethingHAppened"는 이벤트의 이름인 EventHandler 대리자 수행(대리자의 인스턴스 이벤트)
+
+        public void DoSomething (int number)
+        {
+            int temp = number % 10;//43 % 10 = 3 % 3 = 0
+
+            if (temp != 0 && temp % 3 ==0)//3,6, 9  으로 떨어지는 값 (!= 같지않다)
+            {
+                SomethingHAppened($"{number} : 짝!"); // 3, 6, 9에서의 값에서는 짝! 하겠다(이벤트를 사용 로직은 없음)
+            }
+            else
+            {
+                SomethingHAppened($"{number}"); //(이벤트를 사용)
+            }
+        }
+        
+    }
+    class Program
+    {
+
+        //이벤트가 발생했을때 실행되는 메서드 (이벤트 핸들러)
+        public static void MyHandler(string message)
+        {
+            Console.WriteLine(message);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("이벤트 사용!");
+            CustomNotifier notifier = new CustomNotifier();
+            notifier.SomethingHAppened += new EventHandler(MyHandler);//이벤트를 내가 만든 로직이랑 연결
+
+            for (int i = 1; i < 100; i++)
+            {
+                notifier.DoSomething(i);//100 이하에서 3, 6, 9에 관련된 값들만 나타남 
+            }
+        }
+    }
+}
