ed08d71cf6e1d1f8fbb17e6a78d8c092b7218aa6
tjdwn6459 <tjdwn6459@naver.com>
2021-08-11 14:44:00 +0900

    string, bool, array

--- Chap03/ArrayTestApp/ArrayTestApp/Program.cs
+++ Chap03/ArrayTestApp/ArrayTestApp/Program.cs
@@ -14,7 +14,10 @@ namespace ArrayTestApp
             {
                 Console.WriteLine("배열테스트");
 
+                //배열 요소 형식과(int형 배열) 요소 수를 지정하는 new 연산자 사용(따로 초기화 하지 않음)
+                //int ary2[] = {1, 3, 4, 5}일 경우는 ary2의 배열을 선언사 초기화 한다 
                 int[] arr = new int[10];
+                // i < arr.Length 배열의 길이를 설정 (시작점, 배열의 길이, 값 증가)
                 for (var i = 0; i < arr.Length; i++)
                 {
                     arr[i] = i + 1;
--- Chap03/CharTestApp/Program.cs
+++ Chap03/CharTestApp/Program.cs
@@ -10,7 +10,7 @@ namespace CharTestApp
     {
         static void Main(string[] args)
         {
-            // 문자
+            // 문자 /t띄어쓰기 /n개행 
             char ch1 = 't', ch2 = '\t', ch3 = '\n', ch4 = '0';
             Console.WriteLine($"{ch1}, {ch2}, {ch3}, {ch4}");
             char ch5 = '\\';
@@ -20,6 +20,7 @@ namespace CharTestApp
             string str1 = "\a\tHello, \nWorld!"; // \a는 알람소리, \t는 tap, \n은 줄바꿈
             Console.WriteLine(str1); // = Console.WriteLine($"{str1}");
 
+            //값을 사용하며 논리 연산을 수행한다(구조체 형식)
             bool isCorrect = false;
             if (isCorrect)
             {
@@ -28,8 +29,10 @@ namespace CharTestApp
             }
             else
             {
+                //iscorrect의 값이 false이니 거짓이 출력
                 Console.WriteLine("거짓입니다");
             }
+
             Console.WriteLine(isCorrect);
         }
     }
